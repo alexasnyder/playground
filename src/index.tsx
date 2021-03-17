158a1460b9a84f3237ed1e833192e0b2d731e979
Alexa Snyder <asnyder@four51.com>
2021-03-16 19:09:44 -0500

    add GA tracking ID

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
 import history from './services/history';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-L9VE2PKRKE');
 
 ReactDOM.render(
   <React.StrictMode>
