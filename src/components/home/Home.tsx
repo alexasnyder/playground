2f09131ce943273d08c1d8772232961d6ae79d62
Alexa Snyder <asnyder@four51.com>
2021-04-12 20:17:45 -0500

    update home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,34 @@
-import { Typography } from '@material-ui/core';
+import { createStyles, Theme, Typography, withStyles } from '@material-ui/core';
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const styles = (theme: Theme) => 
+    createStyles({
+        listWrapper: {
+            'padding-left': 0,
+            'list-style-position': 'inside',
+        }
+    })
 class Home extends React.Component<any> {
     public render() {
+        const { classes } = this.props;
+
         return (
             <React.Fragment>
-                <Typography variant="h1">Welcome!</Typography>
+                <Typography variant="h2">Hey There!</Typography>
+                <br />
+                <Typography>
+                    Welcome to my personal site I whipped up for kicks and giggles. As you can probably infer, I am <strong>not</strong> a designer. My primary focus right 
+                    now lies solely in backend work (C#/.Net). I started my development career as a front-end developer, so I have this site to keep my skills sharp.
+                    Key features of this site:
+                    <ul className={classes.listWrapper}>
+                        <li>Google ReCAPTCHA</li>
+                        <li>SendGrid integration for emails</li>
+                        <li>Google Analytics</li>
+                    </ul>
+                </Typography>
+                <br />
+                <br />
                 <Typography>
                     UI is hosted on Heroku
                 </Typography>
@@ -15,11 +39,11 @@ class Home extends React.Component<any> {
                     Server is hosted on Azure
                 </Typography>
                 <Typography>
-                <small><a href="https://github.com/alexasnyder/playground-server" target="_blank">View on GitHub</a></small>
+                    <small><a href="https://github.com/alexasnyder/playground-server" target="_blank">View on GitHub</a></small>
                 </Typography>
             </React.Fragment>
         )
     }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
